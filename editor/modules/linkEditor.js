b594376ae62ec97ba7e24a972304ee89eac01358
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-07-10 18:38:01 +0200

    Update linkEditor.js

--- editor/modules/linkEditor.js
+++ editor/modules/linkEditor.js
@@ -1,1 +1,21 @@
+// public/editor/modules/linkEditor.js
+import { saveEdit } from "./firestore.js";
+import { getSelector } from "./utils.js";
+
+export function onChangeLink(ctxMenu, uid, hideMenu) {
+  const el = ctxMenu.target;
+  const url = prompt("Pega la nueva URL:", el.href);
+  if (!url) {
+    hideMenu();
+    return;
+  }
+
+  el.href = url;
+  hideMenu();
+
+  const selector = getSelector(el);
+  saveEdit(uid, selector, "href", url)
+    .then(() => console.log("✔️ Link guardado:", selector, url))
+    .catch(err => console.error("❌ Error guardando link:", err));
+}
 
