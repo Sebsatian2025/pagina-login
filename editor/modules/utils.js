bc40fbe83f741d0205a3d06040b2dc67cf94133a
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-07-10 18:37:25 +0200

    Update utils.js

--- editor/modules/utils.js
+++ editor/modules/utils.js
@@ -1,1 +1,12 @@
+// public/editor/modules/utils.js
+export function getParam(name) {
+  const p = new URLSearchParams(window.location.search).get(name);
+  return p ? decodeURIComponent(p) : "";
+}
+
+export function getSelector(el) {
+  if (el.id) return `#${el.id}`;
+  const idx = Array.prototype.indexOf.call(el.parentNode.children, el) + 1;
+  return `${el.tagName.toLowerCase()}:nth-child(${idx})`;
+}
 
