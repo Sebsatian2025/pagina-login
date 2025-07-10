dd328524eefcd02feacc79be0cacac0f715c4fc2
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-07-10 18:37:00 +0200

    Update auth.js

--- editor/modules/auth.js
+++ editor/modules/auth.js
@@ -1,1 +1,17 @@
+// public/editor/modules/auth.js
+import { initializeApp } 
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } 
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+export function initAuth(firebaseConfig) {
+  const app  = initializeApp(firebaseConfig);
+  return getAuth(app);
+}
+
+export function onUserReady(auth, callback) {
+  onAuthStateChanged(auth, user => {
+    callback(user);
+  });
+}
 
