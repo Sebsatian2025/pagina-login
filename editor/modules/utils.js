df8d1c49e980c3dd162915e5d5fe7326bda18d54
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-07-11 19:49:17 +0200

    Update utils.js

--- editor/modules/utils.js
+++ editor/modules/utils.js
@@ -1,9 +1,3 @@
-// public/editor/modules/utils.js
-
-/**
- * Genera un selector único simplificado para un elemento.
- * Si tiene id → usa #id, sino tagName:nth-child().
- */
 export function getSelector(el) {
   if (el.id) return `#${el.id}`;
   const tag   = el.tagName.toLowerCase();
